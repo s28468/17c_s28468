0956cf78836e51435fdb44a72d813c3a9155626d
Turchyn Kyrylo <s28468@pjwstk.edu.pl>
2024-03-15 12:27:49 +0100

    task2 commit

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 squares = [x**2 for x in range(1, 11)]
 print("List of squares of numbers from 1 to 10:")
 print(squares)
+#task2
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end + 1)]
+start = 1
+end = 10
+print("List of squares from {} to {}: ".format(start, end), generate_squares(start, end))
