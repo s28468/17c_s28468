d8dd1937c1e636d86a76ee7ae845ff5b66f1f05f
Turchyn Kyrylo <s28468@pjwstk.edu.pl>
2024-03-15 12:31:02 +0100

    task5 commit

--- main.py
+++ main.py
@@ -29,3 +29,21 @@ end = 10
 squares = square_gen.generate_squares(start, end)
 print("List of squares from {} to {}: ".format(start, end), squares)
 print("Square roots of the numbers:", square_gen.calculate_square_roots(squares))
+#task5
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        if end < start:
+            raise ValueError("End of the range should be greater than or equal to start.")
+        return [x**2 for x in range(start, end + 1)]
+
+    def calculate_square_roots(self, squares):
+        return [math.sqrt(num) for num in squares]
+
+square_gen = SquareGenerator()
+start = 10
+end = 1
+try:
+    squares = square_gen.generate_squares(start, end)
+    print("List of squares from {} to {}: ".format(start, end), squares)
+except ValueError as ve:
+    print("Error:", ve)
