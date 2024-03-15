5bc69c88678eead686938c749b255dd855d3a878
Turchyn Kyrylo <s28468@pjwstk.edu.pl>
2024-03-15 12:49:16 +0100

    task10 commit

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,5 +1,6 @@
-class SquareGenerator:
+from abc import ABC, abstractmethod
+
+class SquareGenerator(ABC):
+    @abstractmethod
     def generate_squares(self, start, end):
-        if end < start:
-            raise ValueError("End of the range should be greater than or equal to start.")
-        return [x**2 for x in range(start, end + 1)]
+        pass
