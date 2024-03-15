e18f015cea5539185f478f7e9c7fe19fd3d4befc
Turchyn Kyrylo <s28468@pjwstk.edu.pl>
2024-03-15 12:28:08 +0100

    task3 commit

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ def generate_squares(start, end):
 start = 1
 end = 10
 print("List of squares from {} to {}: ".format(start, end), generate_squares(start, end))
+#task3
+class SquareGenerator:
+    def generate_squares(self, start, end):
+        return [x**2 for x in range(start, end + 1)]
+square_gen = SquareGenerator()
+start = 1
+end = 10
+print("List of squares from {} to {}: ".format(start, end), square_gen.generate_squares(start, end))
